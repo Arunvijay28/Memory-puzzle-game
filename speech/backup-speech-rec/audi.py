403404ad52aee62cbdcc4a7c92c4fadda15358a1
Arunvijay28 <arunvijaynov10@gamil.com>
2024-04-08 14:13:18 +0530

    speech updated

--- speech/backup-speech-rec/audi.py
+++ speech/backup-speech-rec/audi.py
@@ -3,13 +3,39 @@ import pyttsx3
 import random
 # recognizer = speech_recognition.Recognizer()
 
-
 engine = pyttsx3.init()
 
 l=['up','down','left','right','red','green','yellow','blue']
 
-for i in range(3):
+arr=[]
+
+def addpattern():
     a=random.randint(0,7)
-    engine.say(l[a])
-    engine.runAndWait()
-print("hi")
+    arr.append(l[a])
+    print(arr)
+
+def playpattern():
+    for i in arr:
+        engine.say(i)
+        engine.runAndWait()
+
+
+def check():
+    for i in arr:
+        user = input("")    # we should get speech input
+        if i != user:
+            print("lose")
+            return False
+    return True
+
+
+while True:
+    addpattern()
+    playpattern()
+    if check():
+        continue
+    else:
+        break
+
+
+        
